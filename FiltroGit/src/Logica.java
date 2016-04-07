1efb243315c63deb5529fa36ee46ee4b41c8767c
AlejandroEndo <alejoendo@hotmail.com>
2016-04-07 09:41:35 -0500

    Corregi el Null

--- FiltroGit/src/Logica.java
+++ FiltroGit/src/Logica.java
@@ -3,30 +3,45 @@ import processing.core.PApplet;
 public class Logica {
 
 	private PApplet app;
-	private FiltroUno fUno = new FiltroUno(app, app.width/2, app.height/2);
-	private FiltroDos fDos = new FiltroDos(app, app.width/2, app.height/2);
-	private FiltroTres fTres = new FiltroTres(app, app.width/2, app.height/2);
-	private FiltroCuatro fCuatro = new FiltroCuatro(app, app.width/2, app.height/2);
-	
+	private FiltroUno fUno;
+	private FiltroDos fDos;
+	private FiltroTres fTres;
+	private FiltroCuatro fCuatro;
+
+	private int color;
+
 	public Logica(PApplet app) {
-		this.app=app;
+		this.app = app;
+
+		fUno = new FiltroUno(app, app.width / 2, app.height / 2);
+		fDos = new FiltroDos(app, app.width / 2, app.height / 2);
+		fTres = new FiltroTres(app, app.width / 2, app.height / 2);
+		fCuatro = new FiltroCuatro(app, app.width / 2, app.height / 2);
+
 	}
-	
-	public void pintar(){
-		
+
+	public void pintar() {
+
+		color += 2;
+		app.fill(color, 100, 100);
+		app.ellipse(app.width / 2, app.height / 2, 50, 50);
+
+		if (color >= 350) {
+			color = 0;
+		}
 		switch (app.key) {
 		case '1':
 			fUno.pintar();
 			break;
-			
+
 		case '2':
 			fDos.pintar();
 			break;
-			
+
 		case '3':
 			fTres.pintar();
 			break;
-			
+
 		case '4':
 			fCuatro.pintar();
 			break;
--- FiltroGit/src/MainApp.java
+++ FiltroGit/src/MainApp.java
@@ -1,15 +1,16 @@
 import processing.core.PApplet;
 
-public class MainApp extends PApplet{
-	
+public class MainApp extends PApplet {
+
 	private Logica logica = new Logica(this);
-	
+
 	@Override
 	public void setup() {
 		size(634, 686);
 		noStroke();
+		 colorMode(HSB, 360, 100, 100);
 	}
-	
+
 	@Override
 	public void draw() {
 		background(0);
